7e1966d1110479beb1465ec4df64c694084dfd20
ryutah <6662577+ryutah@users.noreply.github.com>
2021-12-05 23:32:46 +0900

    feat: adds sprites

---
+++ scripts/create_sprite.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+cd $(dirname $0)/..
+wd=$(pwd)
+
+set -eu
+
+gen_sprite_image() {
+  mkdir -p dist/$1
+  inkscape -w 48 -h 48 source/$1/$1.svg -o dist/$1/$1.png
+}
+export -f gen_sprite_image
+
+gen_sprite() {
+  java -jar $(which plantuml.jar) -encodesprite 16 dist/$1/$1.png >dist/$1/$1.puml
+}
+export -f gen_sprite
+
+clear_dist() {
+  rm -rf ./dist
+  mkdir dist
+}
+
+resize_image() {
+  find ./source |
+    grep '.*\.svg' |
+    sed -E 's/.*\/(.+)\.svg/\1/g' |
+    xargs -I {} bash -c "gen_sprite_image {}"
+}
+
+create_puml() {
+  find ./dist |
+    grep '.*\.png' |
+    sed -E 's/.*\/(.+)\.png/\1/g' | xargs -I {} bash -c "gen_sprite {}"
+}
+
+clear_dist
+resize_image
+create_puml
